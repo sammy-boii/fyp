2bfa6ff7452c2984daa2ebdd1bcf927458306bc8
sammy-boii <neupanesamrajya2005@gmail.com>
2025-12-01 13:52:08 +0545

    fix: build err

--- shared/src/db/prisma.ts
+++ shared/src/db/prisma.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { PrismaClient } from '../prisma/generated/prisma'
+import { PrismaClient } from '../prisma/generated/prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
 const globalForPrisma = global as unknown as {

92d574ee51ca4ef7745eed4046cd395f4bce6ced
sammy-boii <neupanesamrajya2005@gmail.com>
2026-02-08 12:29:50 +0545

    style: change font

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,44 +1,12 @@
 import type { Metadata } from 'next'
-import {
-  Geist,
-  // IBM_Plex_Sans,
-  // JetBrains_Mono,
-  // Orbitron
-  // Space_Grotesk
-  // Space_Grotesk,
-  // Inter,
-  // JetBrains_Mono,
-  // IBM_Plex_Sans,
-  // Orbitron
-} from 'next/font/google'
+import { Space_Grotesk } from 'next/font/google'
 import './globals.css'
 import Provider from '@/lib/provider'
 
-const geistSans = Geist({
+const spaceGrotesk = Space_Grotesk({
   variable: '--font-geist-sans',
   subsets: ['latin']
 })
-
-// const spaceGrotesk = Space_Grotesk({
-//   variable: '--font-space-grotesk',
-//   subsets: ['latin']
-// })
-
-// const jetbrainsMono = JetBrains_Mono({
-//   variable: '--font-jetbrains-mono',
-//   subsets: ['latin']
-// })
-
-// const ibmPlexSans = IBM_Plex_Sans({
-//   variable: '--font-ibm-plex-sans',
-//   weight: ['400', '500', '600', '700'],
-//   subsets: ['latin']
-// })
-
-// const orbitron = Orbitron({
-//   variable: '--font-orbitron',
-//   subsets: ['latin']
-// })
 
 export const metadata: Metadata = {
   title: 'Flux | Automation Application',
@@ -52,7 +20,7 @@ export default function MainLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={`${geistSans.variable} antialiased`}>
+      <body className={`${spaceGrotesk.className} antialiased`}>
         <Provider>{children}</Provider>
       </body>
     </html>
